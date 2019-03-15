51cb58fb94c1bf1887491b7125f6724c5f886281
Richard-D-Taylor <richard.dewayne.taylor@gmail.com>
2019-03-15 18:52:48 -0500

    adds program to remote repo

---
+++ primeNumbers/primeNumbers/main.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+
+using namespace std;
+
+
+//  main.cpp
+//  primeNumbers
+//
+//  Created by Richard Taylor on 3/15/19.
+//  Copyright © 2019 Richard Taylor. All rights reserved.
+//
+
+
+int main()
+{
+    int low, high, i, flag;             
+    
+    cout << "Enter two numbers(intervals): ";
+    cin >> low >> high;
+    
+    cout << "Prime numbers between " << low << " and " << high << " are: ";
+    
+    while (low < high)
+    {
+        flag = 0;
+        
+        for(i = 2; i <= low/2; ++i)
+        {
+            if(low % i == 0)
+            {
+                flag = 1;
+                break;
+            }
+        }
+        
+        if (flag == 0)
+            cout << low << " ";
+        
+        ++low;
+    }
+    
+    return 0;
+}
